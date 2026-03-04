9a69c864d481715a98ee206c1948da03111066eb
finopslatam-sudo <finopslatam@gmail.com>
2026-03-04 19:08:29 -0300

    add aws integration 8

--- app/dashboard/aws/page.tsx
+++ app/dashboard/aws/page.tsx
@@ -207,7 +207,6 @@ export default function AwsIntegrationPage() {
       </div>
 
       {/* ================= INTEGRATION STEPS ================= */}
-      <StepGuide />
 
       <div className="bg-white p-8 rounded-3xl border shadow-xl space-y-6">
 
@@ -222,16 +221,29 @@ export default function AwsIntegrationPage() {
           </li>
 
           <li>
-            Despliega el stack en tu cuenta AWS.
+          Descargar el archivo YAML.
           </li>
 
           <li>
-            FinOpsLatam validará automáticamente el acceso y comenzará la auditoría.
+            Has click en el hipervinculo "Open AWS CloudFormation".
+          </li>
+
+          <li>
+            Copia el contenido numeral de "External ID" lo necesitaras mas adelante.
+          </li>
+
+          <li>
+            Luego sigue los pasos de las imagenes.
           </li>
 
         </ol>
 
       </div>
+
+      {/* ================= STEP GUIDE ================= */}
+
+        <StepGuide />
+
 
       {/* ================= ACTIONS ================= */}
 
@@ -247,7 +259,7 @@ export default function AwsIntegrationPage() {
 
         <ActionCard
         title="Descargar Template"
-        description="Descarga el archivo YAML de CloudFormation manualmente."
+        description="Descarga el archivo YAML de CloudFormation."
         button="Download YAML"
         link="/api/client/aws/template"
         />
